1e671c11dd8ff7b85d95684d78aef97bd33549e5
salkhon <salmankhondker1998@gmail.com>
2024-02-13 22:13:20 +0600

    Updates

--- src/index.ts
+++ src/index.ts
@@ -10,4 +10,3 @@ export * from "./middlewares/current-user";
 export * from "./middlewares/error-handler";
 export * from "./middlewares/require-auth";
 export * from "./middlewares/validate-request";
-
